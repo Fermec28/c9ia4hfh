c7a8ac7f45c80dec24c08a3471640a1d9c4af7f7
Fermedina <fermed28@gmai.com>
2017-12-02 20:09:33 -0500

    solution challenge 5

--- app/models/room.rb
+++ app/models/room.rb
@@ -12,5 +12,12 @@
 #  image_url   :string
 #
 
+#l campo description no debe tener más de 400 caracteres.
+#El campo beds debe ser un número (entero).
+#El campo guests debe ser un número (entero).
+
 class Room < ActiveRecord::Base
+	validates :title, :description, :beds, :guests, :image_url, presence: true
+	validates :description, length:{maximum:400}
+	validates :beds, :guests, numericality: {only_integer: true}
 end
